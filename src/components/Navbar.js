78e5dd91d77b95d4c437632f13fc3adc6bfa80de
Lazaro Moreno <lazmoreno29@gmail.com>
2021-11-23 17:14:12 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+// const Navbar = () => {
+//   return (
+//     <Navbar bg="light" expand="lg">
+//       <>
+//         <Navbar.Brand>
+//           <link to="/">Mixologist</link>
+//         </Navbar.Brand>
+//         <Navbar.Toggle aria-controls="resonsive-navbar-nav" />
+//         <Navbar.Collapse id="responsive-navbar-nav">
+//           <Navbar className="me-auto links">
+//             <link to="/">Home</link>
+//           </Navbar>
+//         </Navbar.Collapse>
+//       </>
+//     </Navbar>
+//   );
+// };
+// export default Navbar;
